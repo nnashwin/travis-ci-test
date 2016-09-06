6505ad27187c407fb39789c9b333f44c6418ec5c
ttymed <tyler.boright@gmail.com>
2016-09-06 13:31:31 +0800

    building master

--- build.sh
+++ build.sh
@@ -19,12 +19,10 @@ echo "install:" >> shippable.yml
 echo " - source ~/.nvm/nvm.sh && nvm install 4.2.3" >> shippable.yml
 echo " - node --version" >> shippable.yml
 echo " - npm config set registry http://registry.npmjs.org/" >> shippable.yml
-echo " - docker pull tetsuoharano/snapaskoceanapi-newhome:1.0" >> shippable.yml
 echo " - docker stop apiServer" >> shippable.yml
 echo " - docker rm apiServer" >> shippable.yml
-echo " - docker run -d -p 3001:3001 --name apiServer tetsuoharano/snapaskoceanapi-newhome:1.0" >> shippable.yml
-echo " - curl 127.0.0.1:3001" >> shippable.yml
-echo " - npm install" >> shippable.yml
+echo " - docker run -d -p 3002:3002 --name apiServer tetsuoharano/node-web-app" >> shippable.yml
+echo " - curl 127.0.0.1:3002" >> shippable.yml
 echo "" >> shippable.yml
 
 echo "# Create directories for test and coverage reports" >> shippable.yml
@@ -46,6 +44,3 @@ git add shippable.yml
 git commit -am 'building master'
 
 git push
-
-rm -rf shippable.yml
-
